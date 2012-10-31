a6e2010688bd1167a61277404283260ca9fef757
prakhar1989 <prakhar1989@gmail.com>
2012-10-31 22:07:05 +0530

    BracketRacket[Intermediate]

---
+++ bracketracket.py
@@ -0,0 +1,17 @@
+"""
+Write a function, where given a string of 
+arbitrary characters, returns true if 
+all brackets (defined as parentheses, square-brackets,
+curly-braces, and chevrons) are correctly paired and ordered.
+This is to say that all brackets,
+if they enclose other brackets, enclose both the 
+paired opening and closing characters.
+"""
+
+def brackets(a):
+    brack_dict = {'(':')','[':']','{':'}','<':'>'}
+    print [brack_dict[i] for i in a if i in '([{<'] 
+    print [i for i in a[::-1] if i in ')]}>']
+    
+print brackets("()abc[])")
+print brackets("([<{abc123abc}>])")
