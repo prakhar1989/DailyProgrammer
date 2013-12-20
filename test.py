527f9139145c083ff2d31c99e5dc0443bbd6d5e7
Prakhar Srivastav <prakhar1989@gmail.com>
2013-12-20 14:16:36 +0300

    Removed file

--- test.py
+++
@@ -1,8 +0,0 @@
-import re
-from sys import argv
-value = "7653"
-layout = ('[abc]', '[def]', '[ghi]', '[jkl]', '[mno]', '[pqrs]', '[tuv]', '[wxyz]')
-keys = ''.join(layout[k-2] for k in map(int, value))
-print keys
-#wlist = open("txtfiles/wordlist.txt", 'r').read()
-# print '\n'.join(re.findall('^%s.*$' % keys, wlist, re.MULTILINE))
